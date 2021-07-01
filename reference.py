7df2a9ce2fc7b81668b9c797083f5581ff9f6722
Mostafa Hussein <tnt.empire00@gmail.com>
2021-06-30 22:05:57 -0400

    Update reference.py

--- reference.py
+++ reference.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import db
 
-cred = firebase_admin.credentials.Certificate('online-chess-6eb6b-firebase-adminsdk-3uoqy-2677834a50.json')
+cred = firebase_admin.credentials.Certificate('online-chess-6eb6b-firebase-adminsdk-3uoqy-be9bbcb25d.json')
 app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://online-chess-6eb6b-default-rtdb.firebaseio.com/'
 })
